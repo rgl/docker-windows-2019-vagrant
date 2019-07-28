9d2e5a6f982890f43b5d6c91fa37e9de90ed2c33
Rui Lopes <rgl@ruilopes.com>
2019-07-28 09:33:07 +0100

    upgrade to portainer 1.21.0

--- images/portainer/run.ps1
+++ images/portainer/run.ps1
@@ -8,7 +8,7 @@ docker `
     -d `
     -v //./pipe/docker_engine://./pipe/docker_engine `
     -p 9000:9000 `
-    portainer:1.20.2 `
+    portainer:1.21.0 `
         -H npipe:////./pipe/docker_engine
 
 $url = 'http://localhost:9000'
